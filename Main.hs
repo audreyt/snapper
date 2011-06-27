6dd075e8d5a3fc069ca9b78984f71e77ce9724a4
Audrey Tang <audrey.tang@socialtext.com>
2011-06-27 20:06:51 +0800

    * /?x=1 now automatically binds <_x/> to "1".

--- Main.hs
+++ Main.hs
@@ -5,14 +5,16 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 
 -- Routes for GET
-get [] = template "main"
+get [] = do
+    set "Title" "Main Page"
+    template "main"
 
 get ["say", msg] = do
-    var "message" msg
+    set "message" msg
     template "echo"
 
 get ("hello":xs) = do
-    mime "text/plain"
+    contentType "text/plain"
     text $ "Hello: " ++ unwords xs
 
 get _ = pass
@@ -31,17 +33,36 @@ post _ = pass
 
 main = snapper routes{ _GET_ = get, _POST_ = post } $ do
     -- Inline templates
-    hamlet "main" [h|
-<ul
-    <li><a href=hello/1/2/3>Hello
-    <li><a href=say/world>world,
-    <li><a href="echo?message=exclamation;foo=bar">(echo)!
-<hr
-<form action=add method=post
-    <input name=x
-    +
-    <input name=y
-    <input type=submit value=Calculate!
+    html "main" [s|
+<ul>
+    <li><a href="hello/1/2/3">Hello</a></li>
+    <li><a href="say/world">world,</a></li>
+    <li><a href="echo?message=exclamation;foo=bar">(echo)!</a></li>
+</ul>
+<hr>
+<form action=add method=post>
+    <input name=x> + <input name=y>
+    <input type=submit value=Calculate!>
+</form>
+|]
+    html "layout/default" [s|
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="/screen.css" />
+    <title><Title/></title>
+  </head>
+  <body>
+    <div id="header">
+      <h1>XYZ Inc.</h1>
+    </div>
+    <div id="content">
+      <content />
+    </div>
+    <div id="footer">
+      <p>Copyright XYZ Inc</p>
+    </div>
+  </body>
+</html>
 |]
     -- External static directory, and dynamic template directory with .tpl/.xtpl files
     return ("static", "templates")
--- Snapper.hs
+++ Snapper.hs
@@ -2,9 +2,10 @@
 module Snapper (
     Routes(..),
     routes, snapper, html, xhtml,
-    var, vars, hasParam, param, template, text, mime,
+    set, var, hasParam, param, template, text,
+    contentType,
 
-    status, header, res, req, hamlet, h, halt,
+    status, header, res, req, hamlet, h, halt, layout,
 
     module Snap.Types,
     module Data.String.QQ,
@@ -31,15 +32,18 @@ import qualified Data.Text.Encoding as E
 import qualified Text.XmlHtml as X
 import qualified Control.Monad.Writer as W
 import Snap.Util.Readable
+import Control.Monad.Reader.Class (asks, local)
+import Control.Monad.IO.Class (liftIO, MonadIO)
 import qualified Text.Hamlet as H
+import qualified Data.HashTable.IO as HT
 
 data Routes m = MonadSnap m => Routes
     { _GET_, _POST_, _HEAD_, _DELETE_, _PUT_ :: [String] -> m ()
     , _before_, _after_ :: m ()
     }
 
-mime :: MonadSnap m => ByteString -> m ()
-mime = modifyResponse . setContentType
+contentType :: MonadSnap m => ByteString -> m ()
+contentType = modifyResponse . setContentType
 
 routes :: MonadSnap m => Routes m
 routes = Routes
@@ -48,9 +52,12 @@ routes = Routes
     where
     nil = const pass
 
-snapper routes templates = quickHttpServe quickInit $ quickSite
+snapper routes templates = quickHttpServe quickInit quickSite
     where
-    quickInit = ApplicationState <$> heistInitializer dynamic applyTemplates
+    quickInit = do
+        ts <- heistInitializer dynamic applyTemplates
+        vs <- io $ HT.new
+        return $ SnapperState{ templateState = ts, variableState = vs }
     ((static, dynamic), templateMap) = W.runWriter templates
     applyTemplates :: Monad m => TemplateState m -> TemplateState m
     applyTemplates = foldl 
@@ -59,17 +66,22 @@ snapper routes templates = quickHttpServe quickInit $ quickSite
         (M.toList templateMap)
     quickSite = do
         req <- getRequest
-        p <- (maybe pass return . urlDecode . rqPathInfo) req
-        let args = U.toString <$> B.splitWith (== 0x2F) p
-        _before_ routes
-        case rqMethod req of 
-            GET -> (_GET_ routes) args <|> template (U.toString p) <|> serveDirectory static
-            HEAD -> (_HEAD_ routes) args <|> template (U.toString p) <|> serveDirectory static
-            POST -> (_POST_ routes) args
-            PUT -> (_PUT_ routes) args
-            DELETE -> (_DELETE_ routes) args
-            _ -> pass
-        _after_ routes
+        vs <- io $ HT.fromList
+            [ ("_" `T.append` E.decodeUtf8 k, E.decodeUtf8 (B.intercalate " " vs))
+            | (k, vs) <- M.toList $ rqParams req
+            ]
+        local (\s -> s{ variableState = vs }) $ do
+            _before_ routes
+            pathInfo <- (maybe pass return . urlDecode . rqPathInfo) req
+            let args = U.toString <$> B.splitWith (== 0x2F) pathInfo
+            case rqMethod req of 
+                GET -> (_GET_ routes) args <|> template (U.toString pathInfo) <|> serveDirectory static
+                HEAD -> (_HEAD_ routes) args <|> template (U.toString pathInfo) <|> serveDirectory static
+                POST -> (_POST_ routes) args
+                PUT -> (_PUT_ routes) args
+                DELETE -> (_DELETE_ routes) args
+                _ -> pass
+            _after_ routes
 
 h = H.hamlet
 
@@ -86,9 +98,16 @@ xhtml k v = case X.parseXML (U.toString k) v of
     Left err  -> fail err
     Right doc -> W.tell $ M.singleton k (X.docContent doc)
 
-var k v = modifyRequest $ rqSetParam k [U.fromString v]
+var :: T.Text -> Snapper String
+var key = do
+    vs  <- asks variableState
+    val <- io $ HT.lookup vs key
+    return $ maybe "" T.unpack val
 
-vars kvs = mapM_ (uncurry var) kvs
+set :: T.Text -> String -> Snapper ()
+set key val = do
+    vs <- asks variableState
+    io $ HT.insert vs key (T.pack val)
 
 hasParam k = do
     x <- getParam k
@@ -99,9 +118,11 @@ param k = do
     x <- getParam k
     maybe pass fromBS x
 
-type Application = SnapExtend ApplicationState
-newtype ApplicationState = ApplicationState { templateState :: HeistState Application }
-
+type Snapper = SnapExtend SnapperState
+data SnapperState = SnapperState
+    { templateState :: !(HeistState Snapper)
+    , variableState :: !(HT.BasicHashTable T.Text T.Text)
+    }
 
 header k v = modifyResponse (addHeader k v)
 
@@ -111,12 +132,24 @@ status = modifyResponse . setResponseCode
 text :: MonadSnap m => String -> m ()
 text = writeBS . U.fromString
 
-template name = do
-    params <- getParams
-    let args = [ (E.decodeUtf8 k, E.decodeUtf8 (B.intercalate " " vs)) | (k, vs) <- M.toList params ]
-    heistLocal (bindStrings args) (render $ U.fromString name)
+layout :: String -> Snapper ()
+layout = set ""
 
-instance HasHeistState Application ApplicationState where
+template :: String -> Snapper ()
+template name = do
+    vs <- asks variableState
+    layout <- io $ HT.lookup vs ""
+    args <- io $ HT.toList vs
+    let layoutTemplate = "layout/" `T.append` maybe "default" id layout
+    let applyLayout nodes = return [X.Element "apply" [("template", layoutTemplate)] nodes]
+        hookLayout state
+            | hasTemplate (E.encodeUtf8 layoutTemplate) state
+            = addPreRunHook applyLayout state
+            | otherwise
+            = state
+    heistLocal (hookLayout . bindStrings args) (render $ U.fromString name)
+
+instance HasHeistState Snapper SnapperState where
     getHeistState     = templateState
     setHeistState s a = a { templateState = s }
 
@@ -128,3 +161,6 @@ req = withRequest . (return .)
 
 halt :: MonadSnap m => m ()
 halt = finishWith =<< getResponse
+
+io :: MonadIO m => IO a -> m a
+io = liftIO
